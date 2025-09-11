61830df432949c8848f8f3614d09c0c80030635d
Ruchi <ruchiarya4215@gmail.com>
2025-09-11 15:40:36 +0530

    create frame shape section

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const inquiryRoutes = require("./routes/inquiry-routes");
 const userRoutes = require("./routes/user-route");
 const disclaimerRoute = require("./routes/disclaimer-routes");
 const eyeServicesRoute = require("./routes/our-eye-services-routes");
+const frameShapesRoute = require("./routes/frame-shapes-route")
 const database = require("./config/config");
 const cors = require("cors");
 
@@ -86,6 +87,7 @@ app.use("/api", inquiryRoutes);
 app.use("/api", userRoutes);
 app.use("/api", disclaimerRoute);
 app.use("/api", eyeServicesRoute);
+app.use("/api", frameShapesRoute);
 
 const PORT = process.env.PORT || 4000;
 
