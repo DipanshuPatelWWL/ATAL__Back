5ea04d1ec2668cccf54a196cf6e27a7d34e4c169
WorldWeblogic <shantanu.kr.worldweblogic@gmail.com>
2025-10-06 19:00:32 +0530

    chat system

--- routes/chat-routes.js
+++ routes/chat-routes.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authMiddleware } = require("../middleware/auth-middleware");
-const { sendMessage, getMessages } = require("../controller/chat-controller");
+const { sendMessage, getMessages, deleteMessage } = require("../controller/chat-controller");
 
-router.post("/send", protect, authMiddleware(["admin", "vendor"]), sendMessage);
-router.get("/chat/:userId", protect, authMiddleware(["admin", "vendor"]), getMessages);
+router.post(
+  "/send",
+  protect,
+  authMiddleware(["admin", "vendor", "company"]),
+  sendMessage
+);
+router.get(
+  "/chat/:userId",
+  protect,
+  authMiddleware(["admin", "vendor", "company"]),
+  getMessages
+);
+
+router.delete(
+  "/message/:id",
+  protect,
+  authMiddleware(["admin", "vendor", "company"]),
+  deleteMessage
+);
 
 module.exports = router;
