4f5f561375f1fd23929146c300533e554a0995e3
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-10-14 13:35:51 +0530

    fix reset password

--- model/user-model.js
+++ model/user-model.js
@@ -1,39 +1,19 @@
 //new user model vai auto generated pass
 
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  profileImage: { type: String },
+  photo: { type: String, default: "" },
+  password: { type: String, required: true }, // will be hashed
+  role: { type: String, enum: ["admin", "vendor", "company"], required: true },
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    photo: { type: String, default: "" },
-    profileImage: { type: String },
-    password: { type: String, required: true }, // will be hashed
-    role: {
-      type: String,
-      enum: ["admin", "vendor", "company"],
-      required: true,
-    },
-
-    profile: {
-      address: String,
-      phone: String,
-      website: String,
-    },
-  },
-  { timestamps: true }
-);
-
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); // only hash if changed
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
+  profile: {
+    address: String,
+    phone: String,
+    website: String,
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
